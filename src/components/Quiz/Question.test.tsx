f42ab0b9e456362c0b37c5301cb29fa830dbe31b
Jason Banks <jasonklein@gmail.com>
2023-03-30 16:30:03 -0400

    Test certain Question component functionality.

---
+++ src/components/Quiz/Question.test.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import { Question, QuestionProps } from "./Question";
+import { QuestionItem } from "../Card";
+
+const formValues = {};
+const setFormValues = jest.fn();
+
+const props = {
+  questionIndex: 0,
+  formValues,
+  setFormValues,
+};
+
+describe("Question", () => {
+  describe("Multiple-choice", () => {
+    const questionItem: QuestionItem = {
+      question: "What is the capital of France?",
+      correctAnswer: "Paris",
+      incorrectAnswers: ["London", "Madrid", "Berlin"],
+    };
+    const questionProps: QuestionProps = {
+      questionItem: questionItem,
+      ...props,
+    };
+
+    it("renders a multiple-choice question correctly", () => {
+      const { getByText, getByLabelText } = render(
+        <Question {...questionProps} />
+      );
+
+      expect(
+        getByText("1. What is the capital of France?")
+      ).toBeInTheDocument();
+      expect(getByLabelText("Paris")).toBeInTheDocument();
+      expect(getByLabelText("London")).toBeInTheDocument();
+      expect(getByLabelText("Madrid")).toBeInTheDocument();
+      expect(getByLabelText("Berlin")).toBeInTheDocument();
+    });
+
+    it("updates the form values when a multiple-choice question is answered", () => {
+      const { getByLabelText } = render(<Question {...questionProps} />);
+
+      fireEvent.click(getByLabelText("Paris"));
+      fireEvent.click(getByLabelText("London"));
+
+      expect(setFormValues).toHaveBeenNthCalledWith(1, {
+        "What is the capital of France?": "Paris",
+      });
+      expect(setFormValues).toHaveBeenNthCalledWith(2, {
+        "What is the capital of France?": "London",
+      });
+    });
+  });
+
+  describe("Fill-in-the-blank", () => {
+    it("renders a fill-in-the-blank question correctly", () => {
+      const questionItem: QuestionItem = {
+        question: "The capital of France is Paris.",
+        correctAnswer: "Paris",
+      };
+      const questionProps = { questionItem: questionItem, ...props };
+      const { getByText, getByLabelText } = render(
+        <Question {...questionProps} />
+      );
+
+      expect(
+        getByText("1. The capital of France is __________.")
+      ).toBeInTheDocument();
+      expect(getByLabelText("Answer")).toBeInTheDocument();
+    });
+  });
+});
