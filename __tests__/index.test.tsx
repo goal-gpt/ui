3ac68e1bcf310759d51f58d861bb420e91252f26
Edmond Tam <edmondtam2@gmail.com>
2023-05-15 20:28:07 +0100

    Remove v0.2 artifacts on main page and clean up tests

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import React from "react";
 
 import Main from "../pages/index";
@@ -22,27 +22,5 @@ describe("Main component", () => {
     const { getByRole } = render(<Main />);
     const headerImage = getByRole("main");
     expect(headerImage).toBeInTheDocument();
-  });
-
-  it("renders the welcome message", () => {
-    const { getByText } = render(<Main />);
-    const welcomeMessage = getByText(
-      "Improving your personal finance skills is a journey. We are glad to be on it with you!"
-    );
-
-    expect(welcomeMessage).toBeInTheDocument();
-  });
-
-  it("hides the welcome message when dismissed", () => {
-    const { queryByText, getByLabelText } = render(<Main />);
-    const closeButton = getByLabelText("close-welcome");
-
-    fireEvent.click(closeButton);
-
-    const welcomeMessage = queryByText(
-      "Improving your personal finance skills is a journey. We are glad to be on it with you!"
-    );
-
-    expect(welcomeMessage).not.toBeInTheDocument();
   });
 });
