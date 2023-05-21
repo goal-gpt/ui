a5351b849109c283284b0bdd7204557040e8b6c5
Edmond Tam <edmondtam2@gmail.com>
2023-05-21 15:37:57 +0100

    Change README and update cookie message

--- src/components/Cookie/Cookie.tsx
+++ src/components/Cookie/Cookie.tsx
@@ -12,9 +12,9 @@ export function Cookie() {
       buttonClasses="bg-primary text-primary-contrast px-4 mx-4 w-24"
       expires={150}
     >
-      This website uses cookies and Local Storage to store your quizzes, nothing
-      else! If you clear your browser&apos;s history then your quizzes will be
-      deleted.
+      This website uses cookies and storage to help you with your financial
+      journey. By using this website, you agree to our use of cookies and
+      storage.
     </CookieConsent>
   );
 }
