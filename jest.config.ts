a34f06cc6fb8f613caea1d7b7b4bc8883745a6e0
Edmond Tam <edmondtam2@gmail.com>
2023-06-02 17:41:46 +0100

    Doesn't seem to work

--- jest.config.ts
+++ jest.config.ts
@@ -11,6 +11,15 @@ const config = {
   // Add more setup options before each test is run
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   testEnvironment: "jest-environment-jsdom",
+  // moduleNameMapper: {
+  //   "react-markdown":
+  //     "<rootDir>/node_modules/react-markdown/react-markdown.min.js",
+  // },
+  // transformIgnorePatterns: ["node_modules/(?!(remark-gfm|react-markdown)/)"],
+  // transform: {
+  //   "^.+\\.(ts|tsx)?$": "ts-jest",
+  //   "^.+\\.(js|jsx)$": "babel-jest",
+  // },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
