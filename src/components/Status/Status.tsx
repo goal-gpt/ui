5e0da22edf6642bff79d93c3f6ecdcf238ab8d83
Jason Banks <jason@eras.fyi>
2023-06-26 14:07:26 +0200

    Display text while backend response loads

--- src/components/Status/Status.tsx
+++ src/components/Status/Status.tsx
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
-import { Col, Row, Spinner } from "react-bootstrap";
+import React, { useContext, useEffect, useState } from "react";
+import { Carousel, Col, Row, Spinner } from "react-bootstrap";
 
 import { QueryStatus } from "../../hooks/useChat";
 import { ChatContext } from "../Chat";
@@ -9,6 +9,30 @@ export function Status() {
   const { chatStatus } = chatContext || {
     chatStatus: QueryStatus.Loading,
   };
+  const items = [
+    "Visit your local library and borrow books for free.",
+    "Check if your library has an online catalog and reserve books ahead of time to save time.",
+    "Join a book club at your library to meet new people and discover new books.",
+    "By borrowing books from the library, you can save money and reduce waste by not buying new books.",
+  ]; // Array of strings to display
+  // const [currentItemIndex, setCurrentItemIndex] = useState(0); // Index of the currently displayed item
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCurrentItemIndex((prevIndex) => (prevIndex + 1) % items.length);
+  //   }, 2000);
+
+  //   // Initial delay for the first item
+  //   const initialDelay = setTimeout(() => {
+  //     setCurrentItemIndex(0); // Set the currentItemIndex to 1 after 1 second
+  //   }, 5000);
+
+  //   // Clean up the interval on component unmount
+  //   return () => {
+  //     clearInterval(interval);
+  //     clearTimeout(initialDelay);
+  //   }
+  // }, [items.length]);
 
   if (chatStatus === QueryStatus.Success || chatStatus === QueryStatus.Idle) {
     return null;
@@ -36,6 +60,16 @@ export function Status() {
           <span className="visually-hidden">Loading...</span>
         </Spinner>
       </Col>
+      <Carousel controls={false} indicators={false} interval={4000} fade>
+        {items.map((item, index) => (
+          <Carousel.Item
+            key={index}
+            style={{ transitionDuration: '1.5s', transitionTimingFunction: '10s ease-out' }}
+          >
+            <p>{item}</p>
+          </Carousel.Item>
+        ))}
+      </Carousel>
     </Row>
   );
 }
