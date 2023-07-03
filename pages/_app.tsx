dd7a11d7696e82d18bb21fa767dd114dada31662
Edmond Tam <edmondtam2@gmail.com>
2023-07-03 20:19:54 +0100

    Change to use next-plausible (#80)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import { Poppins } from "next/font/google";
 import Image from "next/image";
-import Script from "next/script";
+import PlausibleProvider from "next-plausible";
 import { DefaultSeo } from "next-seo";
 import React, { useEffect, useState } from "react";
 import { ToastContainer } from "react-toastify";
@@ -52,54 +52,56 @@ export default function App({
           supabaseClient={supabaseClient}
           initialSession={pageProps.initialSession}
         >
-          <ChatProvider>
-            <main className={`${poppins.className}`}>
-              <Script
-                defer
-                data-domain="eras.fyi"
-                src="https://plausible.io/js/script.outbound-links.tagged-events.js"
-              />
-              <DefaultSeo
-                defaultTitle="eras"
-                titleTemplate="eras | %s"
-                description="eras is your personal finance companion."
-                canonical="https://app.eras.fyi"
-                openGraph={{
-                  title: "eras | your personal finance companion",
-                  type: "website",
-                  locale: "en_US",
-                  url: "https://app.eras.fyi",
-                  site_name: "eras",
-                  images: [
-                    {
-                      url: "https://app.eras.fyi/_ipx/w_640,q_75/%2Feras-logo.png?url=%2Feras-logo.png&w=640&q=75",
-                      width: 200,
-                      height: 140,
-                      alt: "eras logo",
-                    },
-                  ],
-                }}
-                twitter={{
-                  handle: "@erasfyi",
-                  site: "@erasfyi",
-                  cardType: "summary",
-                }}
-              />
-              <div className="background">
-                <Image
-                  src="/man-at-sunrise.jpeg"
-                  alt="man looking at sunrise"
-                  style={{ objectFit: "cover" }}
-                  fill
+          <PlausibleProvider
+            domain="example.com"
+            enabled
+            trackOutboundLinks
+            taggedEvents
+          >
+            <ChatProvider>
+              <main className={`${poppins.className}`}>
+                <DefaultSeo
+                  defaultTitle="eras"
+                  titleTemplate="eras | %s"
+                  description="eras is your personal finance companion."
+                  canonical="https://app.eras.fyi"
+                  openGraph={{
+                    title: "eras | your personal finance companion",
+                    type: "website",
+                    locale: "en_US",
+                    url: "https://app.eras.fyi",
+                    site_name: "eras",
+                    images: [
+                      {
+                        url: "https://app.eras.fyi/_ipx/w_640,q_75/%2Feras-logo.png?url=%2Feras-logo.png&w=640&q=75",
+                        width: 200,
+                        height: 140,
+                        alt: "eras logo",
+                      },
+                    ],
+                  }}
+                  twitter={{
+                    handle: "@erasfyi",
+                    site: "@erasfyi",
+                    cardType: "summary",
+                  }}
                 />
-              </div>
-              <div className="content">
-                <Component {...pageProps} isAuthChecking={isAuthChecking} />
-              </div>
-              <ToastContainer />
-              <Cookie />
-            </main>
-          </ChatProvider>
+                <div className="background">
+                  <Image
+                    src="/man-at-sunrise.jpeg"
+                    alt="man looking at sunrise"
+                    style={{ objectFit: "cover" }}
+                    fill
+                  />
+                </div>
+                <div className="content">
+                  <Component {...pageProps} isAuthChecking={isAuthChecking} />
+                </div>
+                <ToastContainer />
+                <Cookie />
+              </main>
+            </ChatProvider>
+          </PlausibleProvider>
         </SessionContextProvider>
       </QueryClientProvider>
     </>
