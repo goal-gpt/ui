656c9301900575c85756b5948dcd773244ef0f06
Edmond Tam <edmondtam2@gmail.com>
2023-08-14 21:16:34 +0100

    Attempt to remove font warnings and reduce bundled size

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,10 +6,14 @@ import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import { Poppins } from "next/font/google";
+import Head from "next/head";
+import { useRouter } from "next/router";
 import PlausibleProvider from "next-plausible";
 import { DefaultSeo } from "next-seo";
 import React, { useEffect, useState } from "react";
 import { ToastContainer } from "react-toastify";
+
+import { AppConfig } from "@/utils/AppConfig";
 
 import { ChatProvider } from "../src/components/Chat";
 import { Cookie } from "../src/components/Cookie";
@@ -33,6 +37,7 @@ export default function App({
   );
 
   const queryClient = new QueryClient();
+  const router = useRouter();
 
   const [isAuthChecking, setIsAuthChecking] = useState(true);
 
@@ -60,10 +65,42 @@ export default function App({
             taggedEvents
           >
             <ChatProvider>
+              <Head>
+                <meta charSet="UTF-8" key="charset" />
+                <meta
+                  name="viewport"
+                  content="width=device-width,initial-scale=1"
+                  key="viewport"
+                />
+                <link
+                  rel="apple-touch-icon"
+                  href={`${router.basePath}/apple-touch-icon.png`}
+                  key="apple"
+                />
+                <link
+                  rel="icon"
+                  type="image/png"
+                  sizes="32x32"
+                  href={`${router.basePath}/favicon-32x32.png`}
+                  key="icon32"
+                />
+                <link
+                  rel="icon"
+                  type="image/png"
+                  sizes="16x16"
+                  href={`${router.basePath}/favicon-16x16.png`}
+                  key="icon16"
+                />
+                <link
+                  rel="icon"
+                  href={`${router.basePath}/favicon.ico`}
+                  key="favicon"
+                />
+              </Head>
               <DefaultSeo
-                defaultTitle="eras"
-                titleTemplate="eras | %s"
-                description="eras is your personal finance guide."
+                defaultTitle={AppConfig.title}
+                titleTemplate={`${AppConfig.site_name} | %s`}
+                description={AppConfig.description}
                 canonical="https://app.eras.fyi"
                 openGraph={{
                   title: "eras | personal finance guidance",
