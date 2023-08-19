efb0ccee47e8d0962eb56e8987884a07c23367a0
Edmond Tam <edmondtam2@gmail.com>
2023-08-19 16:40:15 +0100

    Fix Plausible import

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
+const { withPlausibleProxy } = require("next-plausible");
+
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer({});
+module.exports = withPlausibleProxy()(withBundleAnalyzer({}));
