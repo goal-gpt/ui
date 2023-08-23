bee1020128a6b438c741720ba0c9fee2fc5f1606
Edmond Tam <edmondtam2@gmail.com>
2023-08-23 23:31:31 +0100

    Adjust rewrites

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
-const { withPlausibleProxy } = require("next-plausible");
-
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withPlausibleProxy()(withBundleAnalyzer({}));
+module.exports = withBundleAnalyzer({
+  async rewrites() {
+    return [
+      {
+        source: "/js/script.js",
+        destination:
+          "https://plausible.io/js/script.outbound-links.tagged-events.js",
+      },
+      {
+        source: "/api/event",
+        destination: "https://plausible.io/api/event",
+      },
+    ];
+  },
+});
