b811eb003ced4629dcb4e20e34301a249889c7aa
Edmond Tam <edmondtam2@gmail.com>
2023-08-27 17:33:10 +0100

    Try to adjust height a bit more

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -12,7 +12,7 @@ export function Layout({ children }: LayoutProps) {
       <div className="fixed left-0 top-0 z-40">
         <Sidebar />
       </div>
-      <div className="fixed left-0 top-12 h-[calc(100vh-3rem)] max-h-screen w-full sm:top-0 sm:h-screen sm:pl-64">
+      <div className="fixed left-0 top-12 h-[calc(100vh-3rem)] max-h-screen min-h-[100dvh] w-full sm:top-0 sm:h-screen sm:pl-64">
         {children}
       </div>
     </div>
